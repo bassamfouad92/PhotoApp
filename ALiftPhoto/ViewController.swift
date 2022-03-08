3cac13708808165c6cf18da5754a66d110a6210f
Bassam <bassam.fouad@10pearls.com>
2022-03-08 13:29:15 +0500

    Initial Commit

---
+++ ALiftPhoto/ViewController.swift
@@ -0,0 +1,14 @@
+//  Created on 08/03/2022.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
